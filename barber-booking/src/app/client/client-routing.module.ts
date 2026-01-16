3c246b2726b030fec8c498624f991dd97c47f982
igormarkovic11 <igormarkovic200054@gmail.com>
2026-01-16 17:40:48 +0100

    implemented page for services

--- barber-booking/src/app/client/client-routing.module.ts
+++ barber-booking/src/app/client/client-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
+import { FavorsComponent } from './pages/favors/favors.component';
 //import { SelectOptionsComponent } from './pages/select-options/select-options.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'favor', component: FavorsComponent },
   //{ path: 'select-options', component: SelectOptionsComponent },
 ];
 
---
+++ barber-booking/src/app/client/pages/favors/favors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-favors',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './favors.component.html',
+  styleUrl: './favors.component.css',
+})
+export class FavorsComponent {}
