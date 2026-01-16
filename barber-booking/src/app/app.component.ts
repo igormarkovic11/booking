5d728b0a4d3806c51cad6c7be011849b8c9c4d1e
igormarkovic11 <igormarkovic200054@gmail.com>
2026-01-16 02:08:24 +0100

    implemented contact page

--- barber-booking/src/app/app.component.ts
+++ barber-booking/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, FooterComponent],
+  imports: [RouterOutlet, ToolbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'barber-booking';
+
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showFooter = !event.urlAfterRedirects.startsWith('/contact');
+      });
+  }
 }
