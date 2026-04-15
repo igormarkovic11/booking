c28da444c376277e56552b7984b89e2c000c470a
igormarkovic11 <igormarkovic200054@gmail.com>
2026-04-15 21:28:47 +0200

    updated import meta env

--- barber-booking/src/app/environments/environment.ts
+++ barber-booking/src/app/environments/environment.ts
@@ -1,3 +1,21 @@
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_FIREBASE_API_KEY: string;
+    readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+    readonly VITE_FIREBASE_PROJECT_ID: string;
+    readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+    readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+    readonly VITE_FIREBASE_APP_ID: string;
+    readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+    readonly VITE_ADMIN_EMAIL: string;
+    readonly VITE_ADMIN_SECRET: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
 export const environment = {
   production: false,
   firebase: {
